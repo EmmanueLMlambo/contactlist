963681b2f0f7333c66b820ae6d8eefb4ca05550d
EmmanueLMlambo <99268240+EmmanueLMlambo@users.noreply.github.com>
2023-02-24 18:27:09 +0200

    Add files via upload

---
+++ contacts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.shortcuts import render
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add-contact/', views.addContact, name='add-contact'),
+    path('profile/<str:pk>', views.contactProfile, name='profile'),
+    path('edit-contact/<str:pk>', views.editContact, name='edit-contact'),
+    path('delete/<str:pk>', views.deleteContact, name='delete')
+]
+
+def contactProfile(request, pk):
+    return render(request, 'contact-profile.html')
